42a9ac2c24de82a1b81e7b38741c2aaad1b3e4b8
Adi Sahar <adi.s@openweb.com>
2020-07-07 19:02:48 +0300

    enable tree-shaking

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+// https://dev.to/lukasbombach/how-to-write-a-tree-shakable-component-library-4ied
+
+import pkg from './package.json';
 import svgr from '@svgr/rollup';
 import url from '@rollup/plugin-url';
 import json from '@rollup/plugin-json';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import filesize from 'rollup-plugin-filesize';
 import commonjs from '@rollup/plugin-commonjs';
@@ -10,30 +13,25 @@ import external from 'rollup-plugin-peer-deps-external';
 
 export default {
   input: 'src/index.js',
-  output: {
-    dir: 'dist',
-    format: 'es',
-    sourcemap: true
-  },
-  // TODO: doesn't work, do we even need `cjs` in entry?
-  // output: [
-  //   {
-  //     dir: 'dist',
-  //     format: 'cjs',
-  //     sourcemap: true
-  //   },
-  //   {
-  //     dir: 'dist',
-  //     format: 'es',
-  //     sourcemap: true
-  //   }
-  // ],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ],
   plugins: [
-    external(),
-    filesize(),
     url(),
     svgr(),
+    json(),
     postcss(),
+    filesize(),
+    external(),
     babel({ exclude: 'node_modules/**' }),
     // https://github.com/rollup/rollup-plugin-node-resolve/issues/107#issuecomment-489646583
     resolve({ preferBuiltins: true, mainFields: ['browser'] }),
@@ -41,26 +39,8 @@ export default {
     commonjs({
       // https://github.com/rollup/rollup-plugin-commonjs/issues/361
       // https://github.com/rollup/rollup-plugin-commonjs#usage-with-symlinks
-      include: /node_modules/, // changed from 'node_modules/**'
-      // left-hand side can be an absolute path, a path
-      // relative to the current directory, or the name
-      // of a module in node_modules
-      namedExports: {
-        'node_modules/react/index.js': [
-          'cloneElement',
-          'createContext',
-          'Component',
-          'createElement'
-        ],
-        'node_modules/react-dom/index.js': ['render', 'hydrate'],
-        'node_modules/react-is/index.js': [
-          'isElement',
-          'isValidElementType',
-          'ForwardRef'
-        ]
-      }
-    }),
-    json()
+      include: /node_modules/ // changed from 'node_modules/**'
+    })
   ],
   external: ['path']
 };
